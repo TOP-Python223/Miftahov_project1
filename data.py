149dab279a27c91aae145a2d16418bd2b9f6abe0
AynurMif <aynur8miftakhov7@gmail.com>
2022-10-01 16:58:15 +0500

    обновил game и data

--- data.py
+++ data.py
@@ -1,19 +1,51 @@
-"""Дополнительный модуль: работа с данными и файлами данных."""
+"""Дополнительный модуль: глобальные переменные и константы."""
 
 # импорт из стандартной библиотеки
+from typing import Sequence
+from numbers import Real
 
 
-# глобальные переменные модуля
-STATS = {'Player1': {'wins': 2, 'ties': 0, 'fails': 0, 'training': True}}
+# глобальные переменные данных
+STATS = {}
+SAVES = {}
+
+PLAYERS = ()
+
+DIM = 3
+RANGE = range(DIM)
+
+TURNS = [[0]*DIM for _ in RANGE]
 
 
-def read_ini() -> bool:
-    """Читает из файлов данные и заполняет структуры данных."""
+# глобальные переменные типов для аннотаций
+Series = Sequence[Real | str]
+Matrix = Sequence[Series]
+TurnCoords = tuple[int, int]
+Score = tuple[dict, dict]
 
 
-def save_ini():
-    pass
+# глобальные константы
+APP_TITLE = 'Крестики-Нолики'
+PROMPT = ' > '
 
+BOT_NAME_EASY = '#1'
+BOT_NAME_HARD = '#2'
 
-def get_player_name():
-    pass
+TOKENS = ('X', 'O')
+WEIGHTS = (1.5, 1)
+
+COMMANDS = {
+    'добавить нового игрока': ('player', 'новый', 'p', 'н'),
+    'выбрать другого игрока': ('another', 'другой', 'a', 'д'),
+    'начать новую партию': ('game', 'партия', 'g', 'п'),
+    'загрузить партию': ('load', 'загрузка', 'l', 'з'),
+    'отобразить статистику': ('stats', 'таблица', 's', 'т'),
+    'изменить размер поля': ('dim', 'размер', 'd', 'р'),
+    'показать справку': ('help', 'справка', 'h', 'с'),
+    'включить режим обучения': ('training', 'обучение', 't', 'о'),
+    'выйти из игры': ('quit', 'выход', 'q', 'в'),
+}
+
+TRAINING_MESSAGES = {
+
+}
